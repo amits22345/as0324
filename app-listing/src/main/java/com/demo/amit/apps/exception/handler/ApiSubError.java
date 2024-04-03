637120296601701a72ef9acba7e01ec26ba54741
amitsharma1910 <amit.sharma1910@gmail.com>
2024-04-03 18:05:06 +0530

    removed app listing

--- app-listing/src/main/java/com/demo/amit/apps/exception/handler/ApiSubError.java
+++
@@ -1,5 +0,0 @@
-package com.demo.amit.apps.exception.handler;
-
-public class ApiSubError {
-
-}
--- app-listing/src/main/java/com/demo/amit/apps/repository/ToolsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.amit.apps.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.demo.amit.apps.model.Tools;
-
-public interface ToolsRepository extends MongoRepository<Tools, String>{
-
-}
